716d766e4984e86b1a4e0d3f73b7c2a23eaacfc4
Danish_Khan <danishkkhan117@gmail.com>
2023-09-21 18:25:31 +0530

    latest Program

---
+++ src/org/ImpPrograms/FahrenheitToCelsius.java
@@ -0,0 +1,21 @@
+package org.ImpPrograms;
+import java.util.*;
+
+public class FahrenheitToCelsius {
+
+	public static void main(String[] args) {
+		
+		 float temperatue;
+		 Scanner in = new Scanner(System.in);
+		 System.out.println("Enter temperatue in Fahrenheit");
+		 temperatue = in.nextInt();
+		 temperatue = ((temperatue - 32)*5)/9;
+		 System.out.println("Temperatue in Celsius = " + temperatue);
+		 }
+		
+
+		
+
+	}
+
+
