f24b4a214527ccbca470822b877f64b0a3a61898
Danish_Khan <danishkkhan117@gmail.com>
2023-09-22 12:49:25 +0530

    latest

---
+++ src/org/ImpPrograms/Info.java
@@ -0,0 +1,18 @@
+package org.ImpPrograms;
+//. How to create Interface in java Program
+
+
+interface Info {
+	static final String language = "Java";
+	public void display();
+}
+class Simple implements Info {
+	public static void main(String []args) {
+		Simple obj = new Simple();
+		obj.display();
+	}
+	// Defining method declared in interface
+	public void display() {
+		System.out.println(language + " is awesome");
+	}
+}
---
+++ src/org/ImpPrograms/Recursion_Technolamror.java
@@ -0,0 +1,16 @@
+package org.ImpPrograms;
+
+//How to create Find Factorial No using Recursion Program in java
+
+public class Recursion_Technolamror {
+	 static int factorial(int n){
+	 if (n == 1)
+	 return 1;
+	 else
+	 return(n * factorial(n-1));
+	 }
+
+	public static void main(String[] args) {
+	System.out.println("Factorial of 5 is: "+factorial(5));
+	}
+	}
