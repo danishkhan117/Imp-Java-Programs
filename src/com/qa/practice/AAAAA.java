67f0be0b84740d9cfa565ccafe121d3edb862602
shahadat khan <khanshahadat459@gmail.com>
2023-11-02 17:31:01 +0530

    new com

--- src/com/qa/practice/AAAAA.java
+++ src/com/qa/practice/AAAAA.java
@@ -12,6 +12,16 @@ public class AAAAA {
 	 * 7. ArrayToArrayList --> D
 	 * 8. Date1 --> D
 	 * 9. Date2 --> D
-	 * 10.DuplicateCharInString -->
+	 * 10.DuplicateCharInString --> D
+	 * 11.Factorial --> D
+	 * 12.FactorialByRecursion --> D
+	 * 13.FahrenheitToCel
+	 * 14.Fibonacci
+	 * 15.FormatSpecifier
+	 * 16.StringReverse
+	 * 17.StringSize
+	 * 18.IteratorDemo
+	 * 19.NumberPadding
+	 * 20.NumberRev
 	 */
 }
